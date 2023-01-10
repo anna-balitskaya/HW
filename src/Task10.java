6d65690528f0f4d8ae9d442af4871a5ec64ea0a7
AnnaBalitskaya <balitskaya.anna1@gmail.com>
2023-01-10 16:22:35 +0300

    factorial

---
+++ src/Task10.java
@@ -0,0 +1,17 @@
+public class Task10 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 5;
+        int n = a + (int) (Math.random() * b);
+        System.out.println("Случайное число: " + n);
+        int tempValue = 1;
+        if (n == 0) {
+            tempValue = 1;
+        } else {
+            for (int i = 1; i <= n; i++) {
+                tempValue = tempValue * i;
+            }
+        }
+        System.out.println(tempValue);
+    }
+}
